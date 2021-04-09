635350adcc5ff94da2813f8e2da6f4cd37426244
Taufikaji <82194288+Taufikaji@users.noreply.github.com>
2021-04-09 14:03:38 +0700

    Add files via upload

---
+++ biodata v3.py
@@ -0,0 +1,5 @@
+nama = input("Masukkan nama: ")
+kelas = input("Masukkan kelas: ")
+kampus = input("Inputkan kampus: ")
+ 
+print ("Halo, Saya %s, saya kelas %s dan kampus saya di %s " % (nama, kelas, kampus))
